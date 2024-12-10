ddc65cfffbc882f89461fba5b68d9f3e4cd223a9
acustic evil <prisha931@gmail.com>
2024-12-10 18:45:09 +0300

    changed styles for better user experience

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="app-container">
     <AddPathSegment @refreshSegments="refreshSegments" />
     <PathSegmentList ref="pathSegmentList" @refreshSegments="refreshSegments"/>
     <UpdatePathSegment ref="updatePathSegment" @refreshSegments="refreshSegments"/>
@@ -36,5 +36,45 @@ export default {
 
 
 <style>
+/* Общий контейнер для всех компонентов */
+.app-container {
+  display: flex;
+  flex-direction: column; /* Компоненты располагаются друг под другом */
+  align-items: stretch;  /* Растягивание компонентов по ширине родителя */
+  gap: 20px; /* Расстояние между компонентами */
+  padding: 20px; /* Внешние отступы */
+  margin: 0 auto; /* Центровка на странице */
+}
+
+
+form {
+  display: flex;
+  flex-direction: row;
+  gap: 10px; /* Расстояние между элементами формы */
+}
+
+label {
+  font-weight: bold;
+}
+
+input,
+select,
+button {
+  padding: 2px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
 </style>
 
